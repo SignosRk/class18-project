13857f0752e26c2b8b74a1eb0746b881d065a2cc
Yaacob <yyaacob0@gmail.com>
2019-04-11 18:31:17 +0200

    added axios to try how to consume api's and validator js to make all validations more readables and easier

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 /* eslint-disable no-console */
+const axios = require('axios');
+
 const app = require('./app');
 
 const PORT = 8080;
+
+const { validateHouse } = require('./validation');
+
+axios.get('http://pastebin.com/raw/zDnaHXWQ').then(res => {
+    console.log(res.data.map(validateHouse));
+});
 
 app.listen(PORT, () =>
     console.log(`App is running on http://localhost:${PORT}`)
